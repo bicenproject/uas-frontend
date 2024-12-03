be3e344f5eca51de3b3edf51a9cce1d94f4661e3
Xyne <hafizhsyaputra676@gmail.com>
2024-12-04 06:50:42 +0700

    new commit

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -138,7 +138,7 @@ const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
          <footer className="bg-white border-t">  
           <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">  
             <p className="text-center text-gray-500">  
-              © {new Date().getFullYear()} Bina Informatika. All rights reserved.  
+              © {new Date().getFullYear()} Hafizh Syahputra. Bina Informatika.  
             </p>  
           </div>  
         </footer>  
